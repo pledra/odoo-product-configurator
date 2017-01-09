bea72cffcfec5b53500a1b395b180f6f0198c6d1
Paul Catinean <paulcatinean@gmail.com>
2017-01-09 20:16:04 +0200

    [FIX] {6} sale order line description uses .name_get instead of .name for variants added by configuration wizard

--- product_configurator_wizard/wizard/product_configurator.py
+++ product_configurator_wizard/wizard/product_configurator.py
@@ -799,7 +799,7 @@ class ProductConfigurator(models.TransientModel):
         so.write({
             'order_line': [(0, 0, {
                 'product_id': variant.id,
-                'name': variant.name
+                'name': variant.name_get()[0][1]
             })]
         })
 
