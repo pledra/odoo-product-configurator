f418fc899a7d7aae92c08e4121f71d8ff48186b6
Paul Catinean <paulcatinean@gmail.com>
2017-04-25 12:06:33 +0300

    Flake8 fixes

--- product_configurator/tests/test_create.py
+++ product_configurator/tests/test_create.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo.tests.common import TransactionCase
+from openerp.tests.common import TransactionCase
 
 
 class ConfigurationCreate(TransactionCase):
@@ -8,35 +8,28 @@ class ConfigurationCreate(TransactionCase):
     def test_create(self):
         """Test configuration item does not make variations"""
 
-        attr_test = self.env['product.attribute'].create(
-            {'name': 'Test',
-             'value_ids': [
-                 (0, 0, {'name': '1'}),
-                 (0, 0, {'name': '2'}),
-                 ],
-             }
-        )
-        test_template = self.env['product.template'].create(
-            {'name': 'Test Configuration',
-             'config_ok': True,
-             'type': 'product',
-             'categ_id': self.env['ir.model.data'].xmlid_to_res_id(
-                 'product.product_category_5'
-             ),
-             'attribute_line_ids': [
-                 (0, 0, {'attribute_id': attr_test.id,
-                         'value_ids': [
-                             (6, 0, attr_test.value_ids.ids),
-                             ],
-                         'required': True,
-                         }),
-                 ]
-             }
-        )
+        attr_test = self.env['product.attribute'].create({
+            'name': 'Test',
+            'value_ids': [
+                (0, 0, {'name': '1'}),
+                (0, 0, {'name': '2'}),
+            ],
+        })
 
-        self.assertEqual(
-            test_template.product_variant_count,
-            0,
-            "Create should not have any variants"
-            )
+        test_template = self.env['product.template'].create({
+            'name': 'Test Configuration',
+            'config_ok': True,
+            'type': 'product',
+            'categ_id': self.env.ref('product.product_category_5').id,
+            'attribute_line_ids': [(0, 0, {
+                'attribute_id': attr_test.id,
+                'value_ids': [
+                    (6, 0, attr_test.value_ids.ids),
+                ],
+                'required': True,
+            }),
+            ]
+        })
 
+        self.assertEqual(test_template.product_variant_count, 0,
+                         "Create should not have any variants")
