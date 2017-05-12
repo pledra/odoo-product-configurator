8a3bc54012ab81eec3142fd6360933311cf662c2
Simon Pessemesse <simon@auneor-conseil.fr>
2017-05-12 14:42:18 +0200

    Fix creation of BOM when create Variant

--- product_configurator_mrp/models/product.py
+++ product_configurator_mrp/models/product.py
@@ -7,12 +7,12 @@ class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
     @api.multi
-    def create_variant(self, value_ids, custom_values=None):
+    def create_get_variant(self, value_ids, custom_values=None):
         """Add bill of matrials to the configured variant."""
         if custom_values is None:
             custom_values = {}
 
-        variant = super(ProductTemplate, self).create_variant(
+        variant = super(ProductTemplate, self).create_get_variant(
             value_ids, custom_values=custom_values
         )
         attr_products = variant.attribute_value_ids.mapped('product_id')
