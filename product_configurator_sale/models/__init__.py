f8a98dff0822c5a0f98733dcc974d005031a31cd
Paul Catinean <paulcatinean@gmail.com>
2017-06-26 17:27:43 +0300

    General fixes and improvements

--- product_configurator_sale/models/__init__.py
+++ product_configurator_sale/models/__init__.py
@@ -1,4 +1,3 @@
 # -*- coding: utf-8 -*-
 
 from . import sale
-from . import product
--- product_configurator_stock/wizard/product_configurator.py
+++ product_configurator_stock/wizard/product_configurator.py
@@ -15,19 +15,29 @@ class ProductConfiguratorLot(models.TransientModel):
 
     @api.multi
     def action_next_step(self):
+        """Run action_config_done for standard (non-configurable) products"""
         if not self.product_tmpl_id.config_ok:
             return self.action_config_done()
         return super(ProductConfiguratorLot, self).action_next_step()
+
+    @api.multi
+    def _extra_line_values(self, product):
+        """ Hook to allow custom line values to be put on the newly
+        created or edited lines."""
+        vals = {'product_id': product.id}
+        if product.config_ok:
+            description = product._get_mako_tmpl_name()
+        else:
+            description = product.display_name
+        vals.update(description=description)
+        return vals
 
     @api.multi
     def action_config_done(self):
         """Parse values and execute final code before closing the wizard"""
         if not self.product_tmpl_id.config_ok:
             variant = self.product_tmpl_id.product_variant_ids[0]
-            line_vals = {
-                'product_id': variant.id,
-                'description': variant.display_name,
-            }
+            line_vals = self._extra_line_values(variant)
             prod_lot = self.env['stock.production.lot'].create(line_vals)
 
             self.unlink()
@@ -42,10 +52,8 @@ class ProductConfiguratorLot(models.TransientModel):
                 ),
                 'res_id': prod_lot.id,
             }
-        custom_vals = {
-            l.attribute_id.id:
-                l.value or l.attachment_ids for l in self.custom_value_ids
-        }
+
+        custom_vals = self.config_session_id._get_custom_vals_dict()
         # This try except is too generic.
         # The create_get_variant routine could effectively fail for
         # a large number of reasons, including bad programming.
@@ -63,10 +71,7 @@ class ProductConfiguratorLot(models.TransientModel):
                 _('Invalid configuration! Please check all '
                   'required steps and fields.')
             )
-        line_vals = {
-            'product_id': variant.id,
-            'description': variant._get_mako_tmpl_name()
-        }
+        line_vals = self._extra_line_values(variant)
         prod_lot = self.env['stock.production.lot'].create(line_vals)
 
         self.unlink()
