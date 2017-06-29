936c30c6fe4906aba2494d2abe3953cf28b0fb23
Paul Catinean <paulcatinean@gmail.com>
2017-06-29 23:43:56 +0300

    Added product_configurator_purchase module

---
+++ product_configurator_purchase/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Product Configurator Purchase',
+    'version': '9.0.1.0.0',
+    'category': 'Generic Modules/Purchase',
+    'summary': 'Product configuration interface for Purchase',
+    'author': 'Pledra',
+    'license': 'AGPL-3',
+    'website': 'http://www.pledra.com/',
+    'depends': ['purchase', 'product_configurator'],
+    "data": [
+        'data/menu_product.xml',
+        'views/purchase_view.xml',
+    ],
+    'demo': [],
+    'images': [],
+    'test': [],
+    'installable': True,
+    'auto_install': False,
+}
