f37b1b1688c66c0edf2055d5afcfc036d138c4fa
Shruti Singh <shruti.singh@bizzappdev.com>
2018-07-09 16:34:09 +0530

    [FIX][T1902]product_configurator_stock : change js code to solve issue related to product-configuration on serial-number's form

--- product_configurator_stock/static/js/form_widgets.js
+++ product_configurator_stock/static/js/form_widgets.js
@@ -4,42 +4,46 @@ odoo.define('product_configurator_lot.FormView', function (require) {
   "use strict";
 
   var core = require('web.core');
-  var ListView = require('web.ListView');
-  var FormView = require('web.FormView');
+  var ListController = require('web.ListController');
+  var FormController = require('web.FormController');
   var _t = core._t;
 
-  ListView.include({
-    render_buttons: function() {
+  ListController.include({
+    renderButtons: function ($node) {
       var self = this;
       this._super.apply(this, arguments); // Sets this.$buttons
-      if(self.model == 'stock.production.lot' && self.dataset.context.custom_create_variant) {
+      if(self.modelName == 'stock.production.lot' && self.initialState.context.custom_create_variant) {
         this.$buttons.find('.o_list_button_add').css('display', 'none')
         this.$buttons.find('.o_list_button_add_custom').css('display', 'inline')
         this.$buttons.on('click', '.o_list_button_add_custom', function(ev) {
           ev.preventDefault();
-          self.rpc("/web/action/load", {action_id: "product_configurator_stock.action_wizard_product_configurator_lot"}).done(function(result) {
-            self.do_action(result);
+          return self._rpc({
+            route: '/web/action/load',
+            params: {action_id: "product_configurator_stock.action_wizard_product_configurator_lot"}
+          }).then(function (action) {
+            self.do_action(action);
           });
-          return false;
         });
       }
     },
   });
 
-  FormView.include({
-    render_buttons: function($node){
+  FormController.include({
+    renderButtons: function ($node){
       var self = this;
       this._super.apply(this, arguments); // Sets this.$buttons
-      if(self.model == 'stock.production.lot' && self.dataset.context.custom_create_variant) {
+      if(self.modelName == 'stock.production.lot' && self.initialState.context.custom_create_variant) {
         this.$buttons.find('.o_form_button_create').css('display', 'none')
         this.$buttons.find('.oe_form_button_create').css('display', 'none')
         this.$buttons.find('.o_form_button_create_custom').css('display', 'inline')
         this.$buttons.on('click', '.o_form_button_create_custom', function(ev) {
           ev.preventDefault();
-          self.rpc("/web/action/load", {action_id: "product_configurator_stock.action_wizard_product_configurator_lot" }).done(function(result) {
-            self.do_action(result);
+          return self._rpc({
+            route: '/web/action/load',
+            params: {action_id: "product_configurator_stock.action_wizard_product_configurator_lot" }
+          }).then(function (action) {
+            self.do_action(action);
           });
-          return false;
         });
       }
     },
