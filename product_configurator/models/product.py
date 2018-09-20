b512d44c38b7f3ee4a0097e348087555c5e5acc9
Lalit Patidar <lalit.patidar@bizzappdev.com>
2018-09-20 14:44:40 +0530

    [IMP]product_configurator: Add reconfigure product button on variant form and kanban view

--- product_configurator/models/product.py
+++ product_configurator/models/product.py
@@ -307,3 +307,31 @@ class ProductProduct(models.Model):
                 product.config_name = product._get_config_name()
             else:
                 product.config_name = product.name
+
+    @api.multi
+    def reconfigure_product(self):
+        """ Creates and launches a product configurator wizard with a linked
+        template and variant in order to re-configure a existing product. It is
+        esetially a shortcut to pre-fill configuration data of a variant"""
+
+        cfg_steps = self.product_tmpl_id.config_step_line_ids
+        active_step = str(cfg_steps[0].id) if cfg_steps else 'configure'
+
+        wizard_obj = self.env['product.configurator']
+        wizard = wizard_obj.create({
+            'product_id': self.id,
+            'state': active_step
+        })
+
+        return {
+            'type': 'ir.actions.act_window',
+            'res_model': 'product.configurator',
+            'name': "Configure Product",
+            'view_mode': 'form',
+            'context': dict(
+                self.env.context,
+                wizard_id=wizard.id,
+            ),
+            'target': 'new',
+            'res_id': wizard.id,
+        }
