ae4c17ccb51c974fcaaef31e9e71dc2a3ae506e5
Shruti Singh <shruti.singh@bizzappdev.com>
2018-09-25 13:29:05 +0530

    [FIX][T1902][new(039)]product_configurator,product_configurator_purchase,product_configurator_sale: call action_next_step to open next step directly

--- product_configurator_purchase/models/purchase.py
+++ product_configurator_purchase/models/purchase.py
@@ -39,27 +39,14 @@ class PurchaseOrderLine(models.Model):
         purchase order line to edit the configurable product"""
 
         wizard_model = 'product.configurator.purchase'
-        cfg_steps = self.product_id.product_tmpl_id.config_step_line_ids
-        active_step = str(cfg_steps[0].id) if cfg_steps else 'configure'
 
         wizard_obj = self.env[wizard_model]
         wizard = wizard_obj.create({
             'product_id': self.product_id.id,
-            'state': active_step,
             'order_id': self.order_id.id,
             'order_line_id': self.id,
         })
-
-        return {
-            'type': 'ir.actions.act_window',
-            'res_model': wizard_model,
-            'name': "Configure Product",
-            'view_mode': 'form',
-            'context': dict(
-                self.env.context,
-                wizard_id=wizard.id,
-                wizard_model=wizard_model
-            ),
-            'target': 'new',
-            'res_id': wizard.id,
-        }
+        action = wizard.action_next_step()
+        del action['context']['view_cache']
+        action['context']['wizard_model'] = wizard_model
+        return action
