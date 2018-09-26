0746420b975a1fc7de72574a36c8da687dd1312a
Shruti Singh <shruti.singh@bizzappdev.com>
2018-09-26 13:25:11 +0530

    [FIX][T1902]product_configurator : change test case, use demo data

--- product_configurator/tests/test_create.py
+++ product_configurator/tests/test_create.py
@@ -6,56 +6,52 @@ class ConfigurationCreate(TransactionCase):
     def setUp(self):
         super(ConfigurationCreate, self).setUp()
 
-        self.ProductAttribute = self.env['product.attribute']
-        self.ProductAttributeValue = self.env['product.attribute.value']
-        self.ProductAttributeLine = self.env['product.attribute.line']
-        self.ProductConfigDomain = self.env['product.config.domain']
-        self.ProductConfigSteps = self.env['product.config.step']
         self.ProductConfWizard = self.env['product.configurator']
-        self.attr_color = self.ProductAttribute.create({
-            'name': 'Color',
-        })
-        self.attr_size = self.ProductAttribute.create({
-            'name': 'Size',
-        })
-        self.attr_width = self.ProductAttribute.create({
-            'name': 'Width',
-        })
-        self.value_blue = self.ProductAttributeValue.create({
-            'name': 'Blue',
-            'attribute_id': self.attr_color.id
-        })
-        self.value_green = self.ProductAttributeValue.create({
-            'name': 'Green',
-            'attribute_id': self.attr_color.id
-        })
-        self.value_S = self.ProductAttributeValue.create({
-            'name': 'S',
-            'attribute_id': self.attr_size.id
-        })
-        self.value_M = self.ProductAttributeValue.create({
-            'name': 'M',
-            'attribute_id': self.attr_size.id
-        })
-        self.value_L = self.ProductAttributeValue.create({
-            'name': 'L',
-            'attribute_id': self.attr_size.id
-        })
-        self.value_A = self.ProductAttributeValue.create({
-            'name': 'A',
-            'attribute_id': self.attr_width.id
-        })
-        self.value_B = self.ProductAttributeValue.create({
-            'name': 'B',
-            'attribute_id': self.attr_width.id
-        })
+        self.config_product = self.env.ref('product_configurator.bmw_2_series')
         self.product_category = self.env.ref('product.product_category_5')
-        self.step_1 = self.ProductConfigSteps.create({
-            'name': 'Step: 1'
-        })
-        self.step_2 = self.ProductConfigSteps.create({
-            'name': 'Step: 2'
-        })
+
+        # attributes
+        self.attr_fuel = self.env.ref(
+            'product_configurator.product_attribute_fuel')
+        self.attr_engine = self.env.ref(
+            'product_configurator.product_attribute_engine')
+        self.attr_color = self.env.ref(
+            'product_configurator.product_attribute_color')
+        self.attr_rims = self.env.ref(
+            'product_configurator.product_attribute_rims')
+        self.attr_model_line = self.env.ref(
+            'product_configurator.product_attribute_model_line')
+        self.attr_tapistry = self.env.ref(
+            'product_configurator.product_attribute_tapistry')
+        self.attr_transmission = self.env.ref(
+            'product_configurator.product_attribute_transmission')
+        self.attr_options = self.env.ref(
+            'product_configurator.product_attribute_options')
+
+        # values
+        self.value_gasoline = self.env.ref(
+            'product_configurator.product_attribute_value_gasoline')
+        self.value_218i = self.env.ref(
+            'product_configurator.product_attribute_value_218i')
+        self.value_220i = self.env.ref(
+            'product_configurator.product_attribute_value_220i')
+        self.value_red = self.env.ref(
+            'product_configurator.product_attribute_value_red')
+        self.value_rims_378 = self.env.ref(
+            'product_configurator.product_attribute_value_rims_378')
+        self.value_sport_line = self.env.ref(
+            'product_configurator.product_attribute_value_sport_line')
+        self.value_model_sport_line = self.env.ref(
+            'product_configurator.product_attribute_value_model_sport_line')
+        self.value_tapistry = self.env.ref(
+            'product_configurator.product_attribute_value_tapistry' +
+            '_oyster_black')
+        self.value_transmission = self.env.ref(
+            'product_configurator.product_attribute_value_steptronic')
+        self.value_options_1 = self.env.ref(
+            'product_configurator.product_attribute_value_smoker_package')
+        self.value_options_2 = self.env.ref(
+            'product_configurator.product_attribute_value_sunroof')
 
     def test_01_create(self):
         """Test configuration item does not make variations"""
@@ -89,93 +85,56 @@ class ConfigurationCreate(TransactionCase):
     def test_02_product_configuration_restriction(self):
         """Values on future steps breaking current wizard view """
 
-        restriction = self.ProductConfigDomain.create({
-            'name': 'restriction-color',
-            'domain_line_ids': [(0, 0, {
-                'attribute_id': self.attr_color.id,
-                'condition': 'in',
-                'value_ids': [(6, 0, [self.value_blue.id])]
-            })]
-        })
-        product = self.env['product.template'].create({
-            'name': 'bug #59',
-            'config_ok': True,
-            'type': 'consu',
-            'categ_id': self.product_category.id,
-            'list_price': '20000',
-        })
-        attr_line1 = self.ProductAttributeLine.create({
-            'attribute_id': self.attr_color.id,
-            'value_ids': [(6, 0, [self.value_blue.id, self.value_green.id])],
-            'required': True,
-            'product_tmpl_id': product.id,
-        })
-        attr_line2 = self.ProductAttributeLine.create({
-            'attribute_id': self.attr_size.id,
-            'value_ids': [
-                (6, 0, [self.value_S.id, self.value_M.id, self.value_L.id])],
-            'required': True,
-            'product_tmpl_id': product.id,
-        })
-        attr_line3 = self.ProductAttributeLine.create({
-            'attribute_id': self.attr_width.id,
-            'value_ids': [(6, 0, [self.value_A.id, self.value_B.id])],
-            'required': True,
-            'product_tmpl_id': product.id,
-        })
-        step1 = {
-            'config_step_id': self.step_1.id,
-            'attribute_line_ids': [(6, 0, [attr_line1.id])]
-        }
-        step2 = {
-            'config_step_id': self.step_2.id,
-            'attribute_line_ids': [(6, 0, [attr_line2.id, attr_line3.id])]
-        }
-        product.write({
-            'config_step_line_ids': [(0, 0, step1), (0, 0, step2)],
-            'config_line_ids': [(0, 0, {
-                'attribute_line_id': attr_line2.id,
-                'value_ids': [(6, 0, [self.value_M.id])],
-                'domain_id': restriction.id
-            })]
-        })
         product_config_wizard = self.ProductConfWizard.create({
-            'product_tmpl_id': product.id,
+            'product_tmpl_id': self.config_product.id,
         })
         product_config_wizard.action_next_step()
-        setattr(
-            product_config_wizard,
-            '__attribute-' + str(self.attr_color.id),
-            str(self.value_blue.id)
-        )
+        product_config_wizard.write({
+            '__attribute-{}'.format(self.attr_fuel.id): self.value_gasoline.id,
+            '__attribute-{}'.format(self.attr_engine.id): self.value_218i.id
+        })
         product_config_wizard.action_next_step()
-        setattr(
-            product_config_wizard,
-            '__attribute-' + str(self.attr_size.id),
-            str(self.value_M.id)
-        )
+        product_config_wizard.write({
+            '__attribute-{}'.format(self.attr_color.id): self.value_red.id,
+            '__attribute-{}'.format(self.attr_rims.id): self.value_rims_378.id
+        })
+        product_config_wizard.action_next_step()
+        product_config_wizard.write({
+            '__attribute-{}'.format(
+                self.attr_model_line.id): self.value_sport_line.id,
+        })
         product_config_wizard.action_previous_step()
-        setattr(
-            product_config_wizard,
-            '__attribute-' + str(self.attr_color.id),
-            str(self.value_green.id)
-        )
+        product_config_wizard.action_previous_step()
+        product_config_wizard.write({
+            '__attribute-{}'.format(self.attr_engine.id): self.value_220i.id,
+        })
         product_config_wizard.action_next_step()
-        setattr(
-            product_config_wizard,
-            '__attribute-' + str(self.attr_size.id),
-            str(self.value_S.id)
-        )
-        setattr(
-            product_config_wizard,
-            '__attribute-' + str(self.attr_width.id),
-            str(self.value_A.id)
-        )
         product_config_wizard.action_next_step()
-        new_variant = product.product_variant_ids.filtered(
+        product_config_wizard.write({
+            '__attribute-{}'.format(
+                self.attr_model_line.id): self.value_model_sport_line.id,
+        })
+        product_config_wizard.action_next_step()
+        product_config_wizard.write({
+            '__attribute-{}'.format(
+                self.attr_tapistry.id): self.value_tapistry.id,
+        })
+        product_config_wizard.action_next_step()
+        product_config_wizard.write({
+            '__attribute-{}'.format(
+                self.attr_transmission.id): self.value_transmission.id,
+            '__attribute-{}'.format(self.attr_options.id): [
+                [6, 0, [self.value_options_1.id, self.value_options_2.id]]]
+        })
+        product_config_wizard.action_next_step()
+        value_ids = self.value_gasoline + self.value_220i + self.value_red\
+            + self.value_rims_378 + self.value_model_sport_line\
+            + self.value_tapistry + self.value_transmission\
+            + self.value_options_1 + self.value_options_2
+        new_variant = self.config_product.product_variant_ids.filtered(
             lambda variant:
             variant.attribute_value_ids
-            == (self.value_green + self.value_S + self.value_A)
+            == value_ids
         )
         self.assertNotEqual(
             new_variant.id,
