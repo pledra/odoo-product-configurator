24a6e0cf648785d665afadff4cbaf120f7641a10
Shruti Singh <shruti.singh@bizzappdev.com>
2018-09-26 19:49:28 +0530

    [FIX][T1902]product_configurator : remove _compute_product_price_extra

--- product_configurator/models/product.py
+++ product_configurator/models/product.py
@@ -158,39 +158,6 @@ class ProductProduct(models.Model):
                       "(identical attribute values)")
                 )
 
-    @api.depends('attribute_value_ids.price_ids.price_extra',
-                 'attribute_value_ids.price_ids.product_tmpl_id')
-    def _compute_product_price_extra(self):
-        """Compute price of configurable products as sum of products related
-        to attribute values picked"""
-        super(ProductProduct, self)._compute_product_price_extra()
-
-        standard_products = self.filtered(lambda x: not x.config_ok)
-        configurable_products = self - standard_products
-        conversions = self._get_conversions_dict()
-        for product in configurable_products:
-            lst_price = product.product_tmpl_id.lst_price
-            value_ids = product.attribute_value_ids.ids
-            # TODO: Merge custom values from products with cfg session
-            # and use same method to retrieve parsed custom val dict
-            # TODO: Move following code where it is needed
-            custom_vals = {}
-            for val in product.value_custom_ids:
-                custom_type = val.attribute_id.custom_type
-                if custom_type in conversions:
-                    try:
-                        custom_vals[val.attribute_id.id] = conversions[
-                            custom_type](val.value)
-                    except Exception:
-                        raise ValidationError(
-                            _("Could not convert custom value '%s' to '%s' on "
-                              "product variant: '%s'" % (val.value,
-                                                         custom_type,
-                                                         product.display_name))
-                        )
-                else:
-                    custom_vals[val.attribute_id.id] = val.value
-
     @api.model
     def _get_config_name(self):
         return self.name
