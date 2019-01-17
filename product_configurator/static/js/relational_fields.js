7092404938cafca8bd963a6bf03a27e7f8010b88
Paul Catinean <paulcatinean@gmail.com>
2019-01-17 16:51:29 +0100

    [FIX] Fixed clickable widget statusbar and properly centering configuration image in wizard

---
+++ product_configurator/static/js/relational_fields.js
@@ -0,0 +1,20 @@
+odoo.define('product_configurator.FieldStatus', function (require) {
+    "use strict";
+
+    var fields = require('web.relational_fields');
+    var FieldStatus = fields.FieldStatus
+
+    FieldStatus.include({
+
+        /* Prase input as string in order to have a clickable statusbar*/
+        _onClickStage: function (e) {
+            if (this.model == 'product.configurator') {
+                this._setValue(String($(e.currentTarget).data("value")));
+            }
+            else {
+                this._super(e);
+            }
+        },
+
+    });
+});
