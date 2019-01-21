0d0eab25172b02003e927f94fb1c253b46114fff
Paul Catinean <paulcatinean@gmail.com>
2019-01-21 22:26:39 +0100

    Updated version in __manifest__ file

--- product_configurator/__manifest__.py
+++ product_configurator/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Product Configurator',
-    'version': '11.0.1.0.7',
+    'version': '11.0.1.0.10',
     'category': 'Generic Modules/Base',
     'summary': 'Base for product configuration interface modules',
     'author': 'Pledra',
