af1e4a35b8b11272ae96867f9c41df4cc1b87480
Paul <paulcatinean@gmail.com>
2019-04-09 14:43:02 +0200

    [IMP] Moved render methods to controller

--- website_product_configurator/controllers/main.py
+++ website_product_configurator/controllers/main.py
@@ -9,9 +9,41 @@ class ProductConfigWebsiteSale(WebsiteSale):
     def product(self, product, category='', search='', **kwargs):
         # Use parent workflow for regular products
         if not product.config_ok:
-            return super(ProductConfigWebsiteSale, self).product(product, category, search, **kwargs)
+            return super(ProductConfigWebsiteSale, self).product(
+                product, category, search, **kwargs
+            )
 
-        # Render the configuration template based on product and configuration session
-        config_form = request.env['product.config.website'].render_form(product)
+        cfg_session_obj = request.env['product.config.session']
+
+        # Retrieve and active configuration session or create a new one
+        cfg_session = cfg_session_obj.create_get_session(product.id)
+
+        # Render the configuration template based on the configuration session
+        config_form = self.render_form(cfg_session)
 
         return config_form
+
+    def get_render_vals(self, cfg_session):
+        """Return dictionary with values required for website template
+        rendering"""
+
+        vals = {
+            'cfg_session': cfg_session,
+            'cfg_step_lines': cfg_session.get_open_step_lines(),
+            'active_step': cfg_session.get_active_step(),
+        }
+
+        return vals
+
+    def render_form(self, cfg_session):
+        """Render the website form for the given template and configuration
+        session"""
+
+        vals = self.get_render_vals()
+        cfg_step_line = vals.get('active_step', vals.get('cfg_step_lines')[:1])
+
+        # TODO: Implement method to select template from step line if
+        # applied or config step if not
+        template = cfg_step_line.get_website_template()
+
+        request.render(template, vals)
