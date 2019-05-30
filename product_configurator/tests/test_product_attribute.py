b8d4c1c21d47005b6c61cf1a976e17c1896b2bb5
Pooja Khandelwal <pooja.khandelwal@bizzappdev.com>
2019-05-30 10:15:15 +0530

    [IMP][#T00513]product_configurator:change variable name

--- product_configurator/tests/test_product_attribute.py
+++ product_configurator/tests/test_product_attribute.py
@@ -4,57 +4,57 @@ class ProductAttributes(TransactionCase):
 
     def setUp(self):
         super(ProductAttributes, self).setUp()
-        self.attributes = self.env.ref('product.product_attribute_1')
+        self.attribute_1 = self.env.ref('product.product_attribute_1')
 
     def test_01_onchange_custome_type(self):
-        self.attributes.min_val = 20
-        self.attributes.max_val = 30
-        self.attributes.custom_type = 'char'
-        self.attributes.onchange_custom_type()
+        self.attribute_1.min_val = 20
+        self.attribute_1.max_val = 30
+        self.attribute_1.custom_type = 'char'
+        self.attribute_1.onchange_custom_type()
         self.assertEqual(
-            self.attributes.min_val,
+            self.attribute_1.min_val,
             0,
             'Min value is False'
         )
         self.assertEqual(
-            self.attributes.max_val,
+            self.attribute_1.max_val,
             0,
             "Max value is False"
         )
 
-        self.attributes.min_val = 20
-        self.attributes.max_val = 30
-        self.attributes.custom_type = 'int'
-        self.attributes.onchange_custom_type()
+        self.attribute_1.min_val = 20
+        self.attribute_1.max_val = 30
+        self.attribute_1.custom_type = 'int'
+        self.attribute_1.onchange_custom_type()
         self.assertEqual(
-            self.attributes.min_val,
+            self.attribute_1.min_val,
             20,
             "Min value is equal to existing min value"
         )
         self.assertEqual(
-            self.attributes.max_val,
+            self.attribute_1.max_val,
             30,
             "Max value is equal to existing max value"
         )
 
-        self.attributes.custom_type = 'float'
-        self.attributes.onchange_custom_type()
+        self.attribute_1.custom_type = 'float'
+        self.attribute_1.onchange_custom_type()
         self.assertEqual(
-            self.attributes.min_val,
+            self.attribute_1.min_val,
             20,
             "Min value is equal to existing min value when type is changed to integer to float"
         )
         self.assertEqual(
-            self.attributes.max_val,
+            self.attribute_1.max_val,
             30,
             "Max value is equal to existing max value when type is changed to integer to float"
         )
 
     def test_02_onchange_val_custom(self):
-        self.attributes.val_custom = False
-        self.attributes.custom_type = 'int'
-        self.attributes.onchange_val_custom_field()
+        self.attribute_1.val_custom = False
+        self.attribute_1.custom_type = 'int'
+        self.attribute_1.onchange_val_custom_field()
         self.assertFalse(
-            self.attributes.custom_type,
+            self.attribute_1.custom_type,
             "custom_type is False"
         )
