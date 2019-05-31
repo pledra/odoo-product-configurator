4aeb1d628db753c9f7875624984abb28e287e9d9
Shruti Singh <shruti.singh@bizzappdev.com>
2019-05-31 13:14:05 +0530

    [IMP]display images for attribute value

--- website_product_configurator/models/product_config.py
+++ website_product_configurator/models/product_config.py
@@ -31,25 +31,3 @@ class ProductConfigSession(models.Model):
         ])
         if sessions_to_remove:
             sessions_to_remove.unlink()
-
-# [{'name': 'csrf_token', 'value': '94934c5f0fbdfaf8305a579c61d6e48f28bf36ado1559044664'},
-# {'name': 'product_tmpl_id', 'value': '39'}, {'name': 'config_session_id', 'value': '322'},
-# {'name': '__attribute-6', 'value': '8'},
-# {'name': '__attribute-7', 'value': ''},
-# {'name': '__attribute-9', 'value': ''},
-# {'name': '__attribute-10', 'value': ''},
-# {'name': '__attribute-8', 'value': ''},
-# {'name': '__attribute-11', 'value': ''},
-# {'name': '__attribute-12', 'value': ''}] __attribute-6
-# updates  {
-#     'value': {'__attribute-6': 8, '__attribute-13': [], 'product_img': None, 'value_ids': [8], 'weight': 210.0, 'price': 25000.0, 'attribute_value_line_ids': []},
-#     'domain': {
-#         '__attribute-6': [('id', 'in', [8, 9])],
-#         '__attribute-7': [('id', 'in', [10, 11, 12, 13, 14])],
-#         '__attribute-8': [('id', 'in', [])],
-#         '__attribute-9': [('id', 'in', [25, 26, 27])],
-#         '__attribute-10': [('id', 'in', [28, 29, 30])],
-#         '__attribute-11': [('id', 'in', [31, 32, 33])],
-#         '__attribute-12': [('id', 'in', [34, 35])],
-#         '__attribute-13': [('id', 'in', [36, 37, 38, 39])]},
-#         'open_cfg_step_lines': ['4', '1', '3', '5']}
