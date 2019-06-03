19869a3bdfcd4c062a736cafaec0b482e0ea4752
Chirag Parmar <chirag.parmar@bizzappdev.com>
2019-06-03 18:40:49 +0530

    [T-00513][ADD] product_configurator_image_builder: add some changes for test-case

--- product_configurator/wizard/product_configurator.py
+++ product_configurator/wizard/product_configurator.py
@@ -359,6 +359,7 @@ class ProductConfigurator(models.TransientModel):
         # launched and is not stored in the database yet
         if not wizard_id:
             return res
+
         # Get the wizard object from the database
         wiz = self.browse(wizard_id)
         active_step_id = wiz.state
@@ -441,6 +442,7 @@ class ProductConfigurator(models.TransientModel):
                         toolbar=False, submenu=False):
         """ Generate view dynamically using attributes stored on the
         product.template"""
+
         if view_type == 'form' and not view_id:
             view_ext_id = 'product_configurator.product_configurator_form'
             view_id = self.env.ref(view_ext_id).id
@@ -480,6 +482,7 @@ class ProductConfigurator(models.TransientModel):
         """ Create the configuration view using the dynamically generated
             fields in fields_get()
         """
+
         field_prefix = self._prefixes.get('field_prefix')
         custom_field_prefix = self._prefixes.get('custom_field_prefix')
 
