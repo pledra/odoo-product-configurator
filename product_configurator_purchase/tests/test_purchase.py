5b1ca470a8f9cc14f46e680b338420960324ab97
Harshil Trivedi <harshil.trivedi@bizzappdev.com>
2019-06-06 18:00:36 +0530

    [ADD][T00513]product_configurator_purchase : create demo data

---
+++ product_configurator_purchase/tests/test_purchase.py
@@ -0,0 +1,46 @@
+from odoo.addons.product_configurator.tests.\
+    product_configurator_test_cases import ProductConfiguratorTestCases
+from odoo.tests.common import TransactionCase
+from datetime import datetime
+
+
+class Purchase(ProductConfiguratorTestCases):
+    def setUp(self):
+        super(Purchase, self).setUp()
+        self.purchaseOrder = self.env['purchase.order']
+        self.purchaseOrderLine = self.env['purchase.order.line']
+        self.resPartner = self.env.ref('product_configurator_purchase.partenr1')
+        self.currency_id = self.env.ref('base.USD')
+        self.company_id = self.env.ref('base.main_company')
+        self.ProductTemplate = self.env.ref('product_configurator.bmw_2_series')
+        self.ProductConfWizard = self.env['product.configurator.purchase']
+
+    def test_00_action_config_start(self):
+        purchase_order_id = self.purchaseOrder.create({
+            'partner_id': self.resPartner.id,
+            'currency_id': self.currency_id.id,
+            'date_order': datetime.now(),
+            'date_planned': datetime.now(),
+            'company_id': self.company_id.id,
+        })
+        context = dict(
+                default_order_id=purchase_order_id.id,
+                wizard_model='product.configurator.purchase',
+        )
+        self.ProductConfWizard = self.env['product.configurator.purchase'].with_context(context)
+        purchase_order_id.action_config_start()
+        self.configure_product = self._configure_product_nxt_step()
+        reconfig_action = purchase_order_id.order_line.reconfigure_product()
+        reconfig_action.get('res_id')
+        self.assertTrue(
+            purchase_order_id.id,
+            'order id not exsits'
+        )
+        self.assertTrue(
+            purchase_order_id.order_line,
+            'order line not exsits'
+        )
+        self.assertTrue(
+            purchase_order_id.order_line.product_id,
+            'product id not exsits'
+        )
