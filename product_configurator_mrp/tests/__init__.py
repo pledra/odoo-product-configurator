39ae22bb3d2fa3d6237dbe99bb09461c172103fd
Harshil Trivedi <harshil.trivedi@bizzappdev.com>
2019-06-29 12:11:39 +0530

    [fix][00513]product_configurator_mrp : changes in testcase

--- product_configurator_mrp/tests/__init__.py
+++ product_configurator_mrp/tests/__init__.py
@@ -1,2 +1,1 @@
 from . import test_mrp
-from . import test_mrp_wizard
--- product_configurator_mrp/tests/test_mrp.py
+++ product_configurator_mrp/tests/test_mrp.py
@@ -84,19 +84,10 @@ class TestMrp(ProductConfiguratorTestCases):
         })
         context = dict(
             self.env.context,
-            default_order_id=self.id,
-            wizard_model='product.configurator.mrp',
-        )
-        mrpProduction.with_context(context)
-        mrpProduction.action_config_start()
-        self._configure_product_nxt_step()
-        mrpProduction.move_raw_ids(
-            self.env.context,
             default_order_id=mrpProduction.id,
             wizard_model='product.configurator.mrp',
         )
-        # mrpProduction.with_context(context)
-        # vals = mrpProduction.action_config_start()
+        mrpProduction.action_config_start()
         self.ProductConfWizard = self.env[
             'product.configurator.mrp'].with_context(context)
         vals = self._configure_product_nxt_step()
