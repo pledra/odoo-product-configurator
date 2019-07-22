90984b862e55feee98109e068051c72c7e05762c
Shruti Singh <shruti.singh@bizzappdev.com>
2019-07-22 18:49:28 +0530

    [FIX]fix test case: remove self.mrpProduction.browse(vals['res_id']) and fix assertEqual in test_01_action_config_start in test_mrp.py

--- product_configurator_mrp/tests/test_mrp.py
+++ product_configurator_mrp/tests/test_mrp.py
@@ -90,10 +90,9 @@ class TestMrp(ProductConfiguratorTestCases):
         mrpProduction.action_config_start()
         self.ProductConfWizard = self.env[
             'product.configurator.mrp'].with_context(context)
-        vals = self._configure_product_nxt_step()
-        # self.mrpProduction.browse(vals['res_id'])
-        # self.assertEqual(
-        #     vals['res_id'],
-        #     mrpProduction.product_id.id,
-        #     'Not Equal'
-        # )
+        self._configure_product_nxt_step()
+        self.assertEqual(
+            self.config_product,
+            mrpProduction.product_id.product_tmpl_id,
+            'Not Equal'
+        )
