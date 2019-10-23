60884469422fc3f5a1746d7fabe7d7e3f0b6b3ff
Chirag Parmar <chirag.parmar@bizzappdev.com>
2019-10-23 12:48:05 +0530

    [IMP][T-00560] product_configurator: make change for custom value

--- product_configurator/wizard/product_configurator.py
+++ product_configurator/wizard/product_configurator.py
@@ -628,9 +628,9 @@ class ProductConfigurator(models.TransientModel):
                 for dependee_field, val_ids in attr_depends.items():
                     if not val_ids:
                         continue
-                    attrs['readonly'].append(
-                        (dependee_field, 'not in', list(val_ids)))
-                    if attr_line.required:
+                    if attr_line.required and not attr_line.custom:
+                        attrs['readonly'].append(
+                            (dependee_field, 'not in', list(val_ids)))
                         attrs['required'].append(
                             (dependee_field, 'in', list(val_ids)))
 
