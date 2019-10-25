c94fb6f334bcf82141094be04c0b96ae3786d9c7
Chirag Parmar <chirag.parmar@bizzappdev.com>
2019-10-25 10:59:16 +0530

    [IMP][T-00560] product_configurator: solve flake8 and add change for custom

--- product_configurator/wizard/product_configurator.py
+++ product_configurator/wizard/product_configurator.py
@@ -627,9 +627,10 @@ class ProductConfigurator(models.TransientModel):
                 for dependee_field, val_ids in attr_depends.items():
                     if not val_ids:
                         continue
-                    attrs['readonly'].append(
-                        (dependee_field, 'not in', list(val_ids)))
-                    if attr_line.required:
+                    if not attr_line.custom:
+                         attrs['readonly'].append(
+                             (dependee_field, 'not in', list(val_ids)))
+                    if attr_line.required and not attr_line.custom:
                         attrs['required'].append(
                             (dependee_field, 'in', list(val_ids)))
 
