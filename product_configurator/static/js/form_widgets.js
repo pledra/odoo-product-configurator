0803a765940dd53f57adef2a58fe21216a0e3c8a
Shruti Singh <shruti.singh@bizzappdev.com>
2020-01-01 11:40:51 +0530

    [FIX][T3186]fix reset button functionality

--- product_configurator/static/js/form_widgets.js
+++ product_configurator/static/js/form_widgets.js
@@ -1,6 +1,7 @@
 /* Add one more option to boolean_button form widget (displayed in the product.template form view) */
 odoo.define('product_configurator.FieldBooleanButton', function (require) {
 "use strict";
+    var FormController = require('web.FormController');
     var core = require('web.core');
     var basic_fields = require('web.basic_fields');
     var registry = require('web.field_registry');
@@ -32,63 +33,22 @@ odoo.define('product_configurator.FieldBooleanButton', function (require) {
             this._setValue(!this.value);
         },
     });
+
+    FormController.include({
+        _onButtonClicked: function (event) {
+            var attrs = event.data.attrs
+            if (attrs.special === 'no_save') {
+                this.canBeSaved = function() {
+                    return true;
+                }
+                var event_no_save = $.extend( true, {}, event );
+                event_no_save.data.attrs.special = false;
+                return this._super(event_no_save);
+            }
+            this._super(event);
+        },
+    });
+
     registry.add('boolean_button', FieldBooleanButton);
     return FieldBooleanButton
 });
-
-
-
-// var FieldToggleBoolean = AbstractField.extend({
-//         description: _lt("Button"),
-//         template: "toggle_button",
-//         events: {
-//                 'click': '_onToggleButton'
-//         },
-//         supportedFieldTypes: ['boolean'],
-
-//         //--------------------------------------------------------------------------
-//         // Public
-//         //--------------------------------------------------------------------------
-
-//         /**
-//          * A boolean field is always set since false is a valid value.
-//          *
-//          * @override
-//          */
-//         isSet: function () {
-//                 return true;
-//         },
-
-//         //--------------------------------------------------------------------------
-//         // Private
-//         //--------------------------------------------------------------------------
-
-//         /**
-//          * @override
-//          * @private
-//          */
-//         _render: function () {
-//                 this.$('i')
-//                         .toggleClass('o_toggle_button_success', !!this.value)
-//                         .toggleClass('text-muted', !this.value);
-//                 var title = this.value ? this.attrs.options.active : this.attrs.options.inactive;
-//                 this.$el.attr('title', title);
-//                 this.$el.attr('aria-pressed', this.value);
-//         },
-
-//         //--------------------------------------------------------------------------
-//         // Handlers
-//         //--------------------------------------------------------------------------
-
-//         /**
-//          * Toggle the button
-//          *
-//          * @private
-//          * @param {MouseEvent} event
-//          */
-//         _onToggleButton: function (event) {
-//                 event.stopPropagation();
-//                 this._setValue(!this.value);
-//         },
-// });
-// });
