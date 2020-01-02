e20afe8c4b3863cba38ca5b4a562f00ee929f3ca
Shruti Singh <shruti.singh@bizzappdev.com>
2020-01-02 17:00:17 +0530

    [IMP][T3199]migrate product_configurator_sale to v13 from v12

--- product_configurator_sale/models/sale.py
+++ product_configurator_sale/models/sale.py
@@ -2,58 +2,66 @@ from odoo import api, fields, models
 
 
 class SaleOrder(models.Model):
-    _inherit = 'sale.order'
+    _inherit = "sale.order"
 
-    @api.multi
     def action_config_start(self):
         """Return action to start configuration wizard"""
-        return {
-            'type': 'ir.actions.act_window',
-            'res_model': 'product.configurator.sale',
-            'name': "Product Configurator",
-            'view_mode': 'form',
-            'target': 'new',
-            'context': dict(
-                self.env.context,
-                default_order_id=self.id,
-                wizard_model='product.configurator.sale',
-            ),
-        }
+        configurator_obj = self.env["product.configurator.sale"]
+        ctx = dict(
+            self.env.context,
+            default_order_id=self.id,
+            wizard_model="product.configurator.sale",
+            allow_preset_selection=True,
+        )
+        return configurator_obj.with_context(ctx).get_wizard_action()
 
 
 class SaleOrderLine(models.Model):
-    _inherit = 'sale.order.line'
+    _inherit = "sale.order.line"
 
     custom_value_ids = fields.One2many(
-        comodel_name='product.attribute.value.custom',
-        inverse_name='product_id',
+        comodel_name="product.attribute.value.custom",
+        inverse_name="product_id",
         related="product_id.value_custom_ids",
-        string="Custom Values"
+        string="Custom Values",
     )
     config_ok = fields.Boolean(
-        related="product_id.config_ok",
-        string="Configurable",
-        readonly=True
+        related="product_id.config_ok", string="Configurable", readonly=True
+    )
+    config_session_id = fields.Many2one(
+        comodel_name="product.config.session", string="Config Session"
     )
 
-    @api.multi
     def reconfigure_product(self):
         """ Creates and launches a product configurator wizard with a linked
         template and variant in order to re-configure a existing product. It is
         esetially a shortcut to pre-fill configuration data of a variant"""
-        wizard_model = 'product.configurator.sale'
+        wizard_model = "product.configurator.sale"
 
         extra_vals = {
-            'order_id': self.order_id.id,
-            'order_line_id': self.id,
-            'product_id': self.product_id.id,
+            "order_id": self.order_id.id,
+            "order_line_id": self.id,
+            "product_id": self.product_id.id,
         }
-
-        self = self.with_context({
-            'default_order_id': self.order_id.id,
-            'default_order_line_id': self.id
-        })
-
+        self = self.with_context(
+            {
+                "default_order_id": self.order_id.id,
+                "default_order_line_id": self.id,
+            }
+        )
         return self.product_id.product_tmpl_id.create_config_wizard(
             model_name=wizard_model, extra_vals=extra_vals
         )
+
+    @api.onchange("product_uom", "product_uom_qty")
+    def product_uom_change(self):
+        if self.config_session_id:
+            account_tax_obj = self.env["account.tax"]
+            self.price_unit = account_tax_obj._fix_tax_included_price_company(
+                self.config_session_id.price,
+                self.product_id.taxes_id,
+                self.tax_id,
+                self.company_id,
+            )
+        else:
+            super(SaleOrderLine, self).product_uom_change()
--- product_configurator_sale/wizard/product_configurator.py
+++ product_configurator_sale/wizard/product_configurator.py
@@ -3,54 +3,52 @@ from odoo import api, fields, models
 
 class ProductConfiguratorSale(models.TransientModel):
 
-    _name = 'product.configurator.sale'
-    _inherit = 'product.configurator'
+    _name = "product.configurator.sale"
+    _inherit = "product.configurator"
+    _description = "Product Configurator Sale"
 
     order_id = fields.Many2one(
-        comodel_name='sale.order',
-        required=True,
-        readonly=True
+        comodel_name="sale.order", required=True, readonly=True
     )
     order_line_id = fields.Many2one(
-        comodel_name='sale.order.line',
-        readonly=True
+        comodel_name="sale.order.line", readonly=True
     )
 
     def _get_order_line_vals(self, product_id):
         """ Hook to allow custom line values to be put on the newly
         created or edited lines."""
 
-        line_vals = {
-            'product_id': product_id,
-            'order_id': self.order_id.id
-        }
-
+        line_vals = {"product_id": product_id, "order_id": self.order_id.id}
         extra_vals = self.order_line_id._prepare_add_missing_fields(line_vals)
         line_vals.update(extra_vals)
+        line_vals.update(
+            {
+                "config_session_id": self.config_session_id.id,
+                "price_unit": self.config_session_id.price,
+            }
+        )
         return line_vals
 
-    @api.multi
     def action_config_done(self):
         """Parse values and execute final code before closing the wizard"""
         res = super(ProductConfiguratorSale, self).action_config_done()
-        if res.get('res_model') == self._name:
+        if res.get("res_model") == self._name:
             return res
-        line_vals = self._get_order_line_vals(res['res_id'])
+        line_vals = self._get_order_line_vals(res["res_id"])
 
-        # To call onchange explicite as write and create
+        # Call onchange explicite as write and create
         # will not trigger onchange automatically
-        order_line_obj = self.env['sale.order.line']
+        order_line_obj = self.env["sale.order.line"]
         specs = order_line_obj._onchange_spec()
-        updates = order_line_obj.onchange(line_vals, ['product_id'], specs)
-        values = updates.get('value', {})
+        updates = order_line_obj.onchange(line_vals, ["product_id"], specs)
+        values = updates.get("value", {})
         for name, val in values.items():
             if isinstance(val, tuple):
                 values[name] = val[0]
-        line_vals.update(values)
+        values.update(line_vals)
 
         if self.order_line_id:
-            self.order_line_id.write(line_vals)
+            self.order_line_id.write(values)
         else:
-            self.order_id.write({'order_line': [(0, 0, line_vals)]})
-
+            self.order_id.write({"order_line": [(0, 0, values)]})
         return
