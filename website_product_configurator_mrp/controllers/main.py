c3601f7d913dd2554bd11a02e75c80ddee06c820
Shruti Singh <shruti.singh@bizzappdev.com>
2020-02-21 11:29:20 +0530

    [FIX][T3202]fix controller cart_update

--- website_product_configurator_mrp/controllers/main.py
+++ website_product_configurator_mrp/controllers/main.py
@@ -1,12 +1,12 @@
 from odoo.http import request
 from odoo import http
 
-from odoo.addons.website_product_configurator.controllers.main import ProductConfigWebsiteSale
-
+from odoo.addons.website_product_configurator.controllers.main import (
+    ProductConfigWebsiteSale,
+)
 
 
 class WebsiteProductConfigMrp(ProductConfigWebsiteSale):
-
     @http.route(
         ["/shop/cart/update"],
         type="http",
@@ -16,31 +16,32 @@ class WebsiteProductConfigMrp(ProductConfigWebsiteSale):
         csrf=False,
     )
     def cart_update(self, product_id, add_qty=1, set_qty=0, **kw):
-        if kw.get('assembly') == 'kit':
-            product = request.env['product.product'].browse(int(product_id))
-            attr_products = product.product_template_attribute_value_ids.mapped('product_attribute_value_id')
-            for product in attr_products.mapped('product_id'):
-                request.website.sale_get_order(force_create=1)._cart_update(
-                    product_id=int(product.id),
+        product = request.env["product.product"].browse(int(product_id))
+        if product.config_ok:
+            return super(WebsiteProductConfigMrp, self).cart_update(
+                product_id=product_id,
+                add_qty=add_qty, set_qty=set_qty, **kw
+            )
+        sale_order = request.website.sale_get_order(force_create=True)
+        if kw.get("assembly") == "kit":
+            attr_value_ids = product.product_template_attribute_value_ids
+            attr_products = attr_value_ids.mapped(
+                "product_attribute_value_id.product_id"
+            )
+            for product_id in attr_products:
+                sale_order._cart_update(
+                    product_id=int(product_id.id),
                     add_qty=add_qty,
                     set_qty=set_qty,
                     config_session_id=kw.get("config_session_id", False)
                 )
         else:
-            product = request.env['product.product'].browse(int(product_id))
-            attr_products = product.product_template_attribute_value_ids.mapped('product_attribute_value_id')
-            request.website.sale_get_order(force_create=1)._cart_update(
+            sale_order._cart_update(
                 product_id=int(product.id),
                 add_qty=add_qty,
                 set_qty=set_qty,
-                config_session_id=kw.get("config_session_id", False)
+                config_session_id=kw.get("config_session_id", False),
             )
+        if kw.get("express"):
+            return request.redirect("/shop/checkout?express=1")
         return request.redirect("/shop/cart")
-
-    def config_vars(self, product_tmpl, active_step=None, data=None):
-        res = super(WebsiteProductConfigMrp, self).config_vars(
-            product_tmpl=product_tmpl, active_step=active_step, data=data)
-        active_step = res.get('active_step')
-        if active_step and active_step.product_tmpl_id != product_tmpl:
-            pass
-        return res
