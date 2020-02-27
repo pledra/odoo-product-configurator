292cbdce206dda14b329a56b1ed4360fb3faa4a2
Shruti Singh <shruti.singh@bizzappdev.com>
2020-02-27 16:22:49 +0530

    [FIX][T3202]now assembly option id available only there are related products exists, fix condition in controller

--- website_product_configurator_mrp/controllers/main.py
+++ website_product_configurator_mrp/controllers/main.py
@@ -17,31 +17,25 @@ class WebsiteProductConfigMrp(ProductConfigWebsiteSale):
     )
     def cart_update(self, product_id, add_qty=1, set_qty=0, **kw):
         product = request.env["product.product"].browse(int(product_id))
-        if product.config_ok:
-            return super(WebsiteProductConfigMrp, self).cart_update(
-                product_id=product_id,
-                add_qty=add_qty, set_qty=set_qty, **kw
-            )
-        sale_order = request.website.sale_get_order(force_create=True)
-        if kw.get("assembly") == "kit":
+        if product.config_ok and kw.get("assembly") == "kit":
             attr_value_ids = product.product_template_attribute_value_ids
             attr_products = attr_value_ids.mapped(
                 "product_attribute_value_id.product_id"
             )
-            for product_id in attr_products:
-                sale_order._cart_update(
-                    product_id=int(product_id.id),
-                    add_qty=add_qty,
-                    set_qty=set_qty,
-                    config_session_id=kw.get("config_session_id", False)
+            if not attr_products:
+                return super(WebsiteProductConfigMrp, self).cart_update(
+                    product_id=product_id,
+                    add_qty=add_qty, set_qty=set_qty, **kw
                 )
+
+            for product_id in attr_products:
+                res = super(ProductConfigWebsiteSale, self).cart_update(
+                    product_id=product_id,
+                    add_qty=add_qty, set_qty=set_qty, **kw
+                )
+            return res
         else:
-            sale_order._cart_update(
-                product_id=int(product.id),
-                add_qty=add_qty,
-                set_qty=set_qty,
-                config_session_id=kw.get("config_session_id", False),
+            return super(WebsiteProductConfigMrp, self).cart_update(
+                product_id=product_id,
+                add_qty=add_qty, set_qty=set_qty, **kw
             )
-        if kw.get("express"):
-            return request.redirect("/shop/checkout?express=1")
-        return request.redirect("/shop/cart")
